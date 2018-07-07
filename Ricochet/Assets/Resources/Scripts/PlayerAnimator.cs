3ba5026bc5d5094795321f14b4bc0683808c2090
Nicholas Hallman <ngahallman@gmail.com>
2018-07-07 16:06:03 -0400

    animation stuff

---
+++ Ricochet/Assets/Resources/Scripts/PlayerAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ha = Input.GetAxis("Horizontal");
+        float va = Input.GetAxis("Vertical");
+
+        if (ha > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 0);
+            anim.Play(Animator.StringToHash("Run"));
+        }
+        else if (ha < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 0);
+            anim.Play(Animator.StringToHash("Run"));
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 0);
+            anim.Play(Animator.StringToHash("Stand"));
+        }
+        
+        
+    }
+}
