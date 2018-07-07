7f09af600ecd4e3e359550a5967e93284bb97c2d
Nicholas Hallman <ngahallman@gmail.com>
2018-07-07 17:28:13 -0400

    added more animations

--- Ricochet/Assets/Resources/Scripts/PlayerAnimator.cs
+++ Ricochet/Assets/Resources/Scripts/PlayerAnimator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour {
-
+    bool grounded = true;
     Animator anim;
 	// Use this for initialization
 	void Start () {
@@ -15,22 +15,48 @@ public class PlayerAnimator : MonoBehaviour {
         float ha = Input.GetAxisRaw("Horizontal");
         float va = Input.GetAxisRaw("Vertical");
 
-        if (ha > 0)
+        if (ha > 0 && grounded)
         {
             transform.localScale = new Vector3(1, 1, 0);
             anim.Play(Animator.StringToHash("Run"));
         }
-        else if (ha < 0)
+        else if (ha < 0 && grounded)
         {
             transform.localScale = new Vector3(-1, 1, 0);
             anim.Play(Animator.StringToHash("Run"));
         }
+        else if (!grounded)
+        {
+            anim.Play(Animator.StringToHash("Jump"));
+        }
         else
         {
-            transform.localScale = new Vector3(1, 1, 0);
             anim.Play(Animator.StringToHash("Stand"));
         }
+        if(va != 0)
+        {
+            transform.localScale = new Vector3(ha, 1, 0);
+        }
         
-        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            grounded = true;
+
+        }
+        if(collision.gameObject.name == "Wall")
+        {
+            Debug.Log("wow");
+            gameObject.transform.localScale = new Vector3(transform.localScale.x, 1, 0);
+            anim.Play(Animator.StringToHash("Slide"));
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Floor") grounded = false;
     }
 }
